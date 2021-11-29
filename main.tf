35bd027dddb24f4aa2b0470181448e9d71f84115
Vladislav Naumov <vnaumov@mirantis.com>
2021-11-29 23:45:07 +0100

    [refactor][cicd] eqv2 day0 terraform output vars declaration
    - declare locals block logically right before using desired
      variables
    
    PRODX-19931
    [disable-os-demo]
    [disable-bm-demo]
    [disable-vsphere-demo]
    
    
    Change-Id: I94631fa44edae2c695c8e989e8f89cab3bc6e822
    Reviewed-on: https://gerrit.mcp.mirantis.com/c/kaas/core/+/117878
    Tested-by: Vladislav Naumov <vnaumov@mirantis.com>
    Reviewed-by: Vladislav Naumov <vnaumov@mirantis.com>
    Reviewed-by: Yuriy Taraday <ytaraday@mirantis.com>
    Workflow: Vladislav Naumov <vnaumov@mirantis.com>

--- main.tf
+++ main.tf
@@ -1,4 +1,56 @@
-# VLAN management ##########################################
+# SSH Keys management ##########################################
+# Create SSH Keys if they are not exist
+
+resource "metal_project_ssh_key" "ssh_key_object" {
+  count = var.use_existing_ssh_key_name == "" ? 1 : 0
+
+  name       = "mcc_infra_${var.edge_hostname}"
+  public_key = file(abspath(var.ssh_public_key_path))
+  project_id = var.project_id
+}
+
+# Get Project SSH Key by name
+data "metal_project_ssh_key" "infra_ssh" {
+  depends_on = [metal_project_ssh_key.ssh_key_object]
+
+  search     = var.use_existing_ssh_key_name != "" ? var.use_existing_ssh_key_name : "mcc_infra_${var.edge_hostname}"
+  project_id = var.project_id
+}
+
+# Routers management ##########################################
+
+# Create routers in each configured metro
+resource "metal_device" "edge" {
+  hostname            = "mcc-edge-router-${var.edge_hostname}"
+  plan                = var.edge_size
+  metro               = var.metro
+  operating_system    = var.edge_os
+  billing_cycle       = var.billing_cycle
+  project_id          = var.project_id
+  project_ssh_key_ids = [data.metal_project_ssh_key.infra_ssh.id]
+}
+
+# Change network mode to hybrid for the edge instance
+resource "metal_device_network_type" "edge" {
+  device_id = metal_device.edge.id
+  type      = "hybrid"
+}
+
+locals {
+  routers_meta = {
+    "${var.metro}" = {
+      metro        = var.metro
+      vlan_subnet  = "192.168.0.0/20"
+      private_addr = metal_device.edge.access_private_ipv4,
+      public_addr  = metal_device.edge.access_public_ipv4,
+      device_id    = metal_device.edge.id
+      port_id      = metal_device_network_type.edge.id
+    }
+  }
+}
+
+
+# VLANs management ##########################################
 locals {
   vlan_meta = [
     for i in range(var.vlans_amount) : {
@@ -22,70 +74,6 @@ locals {
         mcc_regional = vlan.mcc_regional
       }
   ] }
-
-  seed_meta = {
-    deploy      = var.deploy_seed
-    addr        = cidrhost("${local.vlan_meta[0].subnet}/24", 2)
-    public_addr = join("", metal_device.seed.*.access_public_ipv4)
-    mask        = local.vlan_meta[0].mask
-    vlan_id     = local.vlan_meta[0].vnid
-    static_route = {
-      subnet   = "192.168.0.0/16"
-      next_hop = local.vlan_meta[0].router_addr
-    }
-  }
-
-  seed_nodes = {
-    for device in metal_device.seed :
-    (device.access_public_ipv4) => {
-      metro        = var.metro
-      addr         = local.seed_meta.addr
-      mask         = local.seed_meta.mask
-      vlan_id      = local.seed_meta.vlan_id
-      static_route = local.seed_meta.static_route
-      public_addr  = device.access_public_ipv4
-  } }
-
-  routers_meta = {
-    "${var.metro}" = {
-      metro        = var.metro
-      vlan_subnet  = "192.168.0.0/20"
-      private_addr = metal_device.edge.access_private_ipv4,
-      public_addr  = metal_device.edge.access_public_ipv4,
-      device_id    = metal_device.edge.id
-      port_id      = metal_device_network_type.edge.id
-    }
-  }
-
-  routers = {
-    for name, router in local.routers_meta :
-    (router.public_addr) => {
-      metro        = router.metro
-      private_addr = router.private_addr
-      public_addr  = router.public_addr
-      vlans = [
-        for vlan in local.vlans[name] : {
-          vlan_id      = vlan.vlan_id
-          router_addr  = vlan.router_addr
-          mask         = vlan.mask
-          subnet       = vlan.subnet
-          mcc_regional = vlan.mcc_regional
-      }]
-  } }
-
-  inventory_file = "ansible-hosts.ini"
-}
-
-output "routers" {
-  value = local.routers
-}
-
-output "seed_nodes" {
-  value = local.seed_nodes
-}
-
-output "vlans" {
-  value = local.vlans
 }
 
 resource "metal_vlan" "mcc_vlan" {
@@ -94,42 +82,6 @@ resource "metal_vlan" "mcc_vlan" {
 
   count       = var.vlans_amount
   description = "mcc_${var.edge_hostname}_192.168.${count.index + 1}.0"
-}
-
-# VLAN management ##########################################
-
-# Edge router #########################################
-
-resource "metal_project_ssh_key" "ssh_key_object" {
-  count = var.use_existing_ssh_key_name == "" ? 1 : 0
-
-  name       = "mcc_infra_${var.edge_hostname}"
-  public_key = file(abspath(var.ssh_public_key_path))
-  project_id = var.project_id
-}
-
-# Get Project SSH Key by name
-data "metal_project_ssh_key" "infra_ssh" {
-  depends_on = [metal_project_ssh_key.ssh_key_object]
-
-  search     = var.use_existing_ssh_key_name != "" ? var.use_existing_ssh_key_name : "mcc_infra_${var.edge_hostname}"
-  project_id = var.project_id
-}
-
-resource "metal_device" "edge" {
-  hostname            = "mcc-edge-router-${var.edge_hostname}"
-  plan                = var.edge_size
-  metro               = var.metro
-  operating_system    = var.edge_os
-  billing_cycle       = var.billing_cycle
-  project_id          = var.project_id
-  project_ssh_key_ids = [data.metal_project_ssh_key.infra_ssh.id]
-}
-
-# Change network mode to hybrid for the edge instance
-resource "metal_device_network_type" "edge" {
-  device_id = metal_device.edge.id
-  type      = "hybrid"
 }
 
 # Attach vlans to the edge instance
@@ -142,7 +94,26 @@ resource "metal_port_vlan_attachment" "vlan_to_router" {
   vlan_vnid = metal_vlan.mcc_vlan[count.index].vxlan
 }
 
-# Seed node #########################################
+output "vlans" {
+  value = local.vlans
+}
+
+# Seed nodes management ##########################################
+
+locals {
+  seed_meta = {
+    deploy      = var.deploy_seed
+    addr        = cidrhost("${local.vlan_meta[0].subnet}/24", 2)
+    public_addr = join("", metal_device.seed.*.access_public_ipv4)
+    mask        = local.vlan_meta[0].mask
+    vlan_id     = local.vlan_meta[0].vnid
+    static_route = {
+      subnet   = "192.168.0.0/16"
+      next_hop = local.vlan_meta[0].router_addr
+    }
+  }
+}
+
 resource "metal_device" "seed" {
   count = var.deploy_seed ? 1 : 0
 
@@ -165,6 +136,19 @@ resource "metal_device" "seed" {
   }
 }
 
+locals {
+  seed_nodes = {
+    for device in metal_device.seed :
+    (device.access_public_ipv4) => {
+      metro        = var.metro
+      addr         = local.seed_meta.addr
+      mask         = local.seed_meta.mask
+      vlan_id      = local.seed_meta.vlan_id
+      static_route = local.seed_meta.static_route
+      public_addr  = device.access_public_ipv4
+  } }
+}
+
 # Change network mode to hybrid for the seed instance
 resource "metal_device_network_type" "seed_network" {
   count = var.deploy_seed ? 1 : 0
@@ -183,6 +167,38 @@ resource "metal_port_vlan_attachment" "vlan_to_seed" {
   port_name  = "bond0"
   # first vlan is mgmt related by default
   vlan_vnid = local.vlan_meta[0].vnid
+}
+
+output "seed_nodes" {
+  value = local.seed_nodes
+}
+
+# Output management ##########################################
+locals {
+  routers = {
+    for name, router in local.routers_meta :
+    (router.public_addr) => {
+      metro        = router.metro
+      private_addr = router.private_addr
+      public_addr  = router.public_addr
+      vlans = [
+        for vlan in local.vlans[name] : {
+          vlan_id      = vlan.vlan_id
+          router_addr  = vlan.router_addr
+          mask         = vlan.mask
+          subnet       = vlan.subnet
+          mcc_regional = vlan.mcc_regional
+      }]
+  } }
+}
+
+output "routers" {
+  value = local.routers
+}
+# Output management ##########################################
+
+locals {
+  inventory_file = "ansible-hosts.ini"
 }
 
 resource "local_file" "ansible-inventory" {
